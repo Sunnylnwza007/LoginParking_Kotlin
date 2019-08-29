e101096a83796729fe326fcac3fb323810c61316
Sunny <trinnakrit@gmail.com>
2019-08-29 18:27:44 +0700

    add login fragment

---
+++ app/src/main/java/buu/informatics/s59160090/loginparking/LoginFragment.kt
@@ -0,0 +1,29 @@
+package buu.informatics.s59160090.loginparking
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import buu.informatics.s59160090.loginparking.databinding.FragmentLoginBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+            R.layout.fragment_login,container,false)
+        return binding.root
+    }
+
+
+}
